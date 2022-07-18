fa6e1155b4bf57ce0ba3a3f93c8d24ac58f5fd9d
paul <paul.boes@porsche.digital>
2022-07-18 16:29:34 +0200

    implement pac rpad algorithms

--- branch_and_bound/BranchAndBoundSolver.py
+++ branch_and_bound/BranchAndBoundSolver.py
@@ -1,27 +1,7 @@
 import numpy as np
 import pybnb
 
-
-class AxisAlignedHyperRectangle:
-    def __init__(self, intervals: np.ndarray):
-        assert intervals.shape[1] == 2, "Unexpected shape of argument"
-        # self.dimension = intervals.shape[1]
-        self.intervals = intervals
-        self.area = self.calculate_area()
-
-    def calculate_area(self):
-        return np.prod(np.apply_along_axis(lambda i: i[1] - i[0], axis=1, arr=self.intervals))
-
-    def update_interval(self, index: int, new_interval: np.ndarray) -> None:
-        self.intervals[index, :] = new_interval
-
-    def contains(self, point: np.ndarray) -> bool:
-        ints = self.intervals
-        return all((ints[:, 0] <= point.T) & (point.T <= ints[:, 1]))
-
-    def copy(self):
-        return AxisAlignedHyperRectangle(self.intervals)
-
+from patterns import AxisAlignedHyperRectangle, find_bounding_pattern
 
 class BranchAndBoundSolver(pybnb.Problem):
     def __init__(self, training_data: np.ndarray, point_to_be_classified: np.ndarray):
@@ -30,7 +10,9 @@ class BranchAndBoundSolver(pybnb.Problem):
         :param point_to_be_classified: a single point as an ndarray of shape (1,d).
         """
         self.points = training_data
-        self.pattern: AxisAlignedHyperRectangle = self.find_bounding_pattern(self.points)
+        self.N = len(training_data)
+        self.pattern: AxisAlignedHyperRectangle = find_bounding_pattern(self.points)
+        self.bounding_area = self.pattern.area
         self.point_to_be_classified = point_to_be_classified
         self.trajectory = []
 
@@ -38,10 +20,10 @@ class BranchAndBoundSolver(pybnb.Problem):
         return pybnb.minimize
 
     def objective(self):
-        return np.float(len(self.points) / self.pattern.area)
+        return np.float(1. * len(self.points) / (self.pattern.area * self.N))
 
     def bound(self):
-        return np.float(1. / self.pattern.area)
+        return np.float(1. * self.bounding_area / (self.pattern.area * self.N))
 
     def save_state(self, node):
         node.state = (self.points,
@@ -61,22 +43,32 @@ class BranchAndBoundSolver(pybnb.Problem):
         for i, interval in enumerate(self.pattern.intervals):
             # branch on the left side
             child_points = np.delete(self.points, argsort[0, i], 0)
-            child_pattern = self.find_bounding_pattern(child_points)
+            child_pattern = find_bounding_pattern(child_points)
+            # new_left_bound = self.points[argsort[1, i], i]
+            # assert new_left_bound >= interval[0]
+            # if new_left_bound < interval[1]:
+            #     child_pattern = self.pattern.copy()
+            #     child_pattern.update_interval(i, np.array([new_left_bound, interval[1]]))
             if child_pattern.contains(self.point_to_be_classified) and child_pattern.area > 0:
                 child = pybnb.Node()
                 child.state = (child_points, child_pattern, self.point_to_be_classified)
                 yield child
             # then on the right
             child_points = np.delete(self.points, argsort[-1, i], 0)
-            child_pattern = self.find_bounding_pattern(child_points)
+            child_pattern = find_bounding_pattern(child_points)
             if child_pattern.contains(self.point_to_be_classified) and child_pattern.area > 0:
                 child = pybnb.Node()
                 child.state = (child_points, child_pattern, self.point_to_be_classified)
                 yield child
-
-    def find_bounding_pattern(self, points: np.ndarray) -> AxisAlignedHyperRectangle:
-        intervals = np.sort(points, axis=0)[[0, -1], :]
-        return AxisAlignedHyperRectangle(intervals.T)
+            # new_right_bound = self.points[argsort[-2, i], i]
+            # assert new_right_bound <= interval[1]
+            # if new_right_bound > interval[0]:
+            #     child_pattern = self.pattern.copy()
+            #     child_pattern.update_interval(i, np.array([interval[0], new_right_bound]))
+            #     assert child_pattern.area <= self.pattern.area
+            #     child = pybnb.Node()
+            #     child.state = (child_points, child_pattern, self.point_to_be_classified)
+            #     yield child
 
     # Reporting method
     def notify_new_best_node(self, node, current):
