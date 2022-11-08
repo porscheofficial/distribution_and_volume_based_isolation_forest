5cc8f1a18cd24e71b280f6156770eb024ac2f0e0
paul <paul.boes@porsche.digital>
2022-11-08 15:45:18 +0100

    added patternspace class and adapted tests

--- rare_pattern_detect/rare_pattern_detect.py
+++ rare_pattern_detect/rare_pattern_detect.py
@@ -1,14 +1,17 @@
-from minlp_based import has_rare_pattern
+from rare_pattern_detect.patterns import PatternSpaceType
+from rare_pattern_detect.minlp_based import minlp_has_rare_pattern
 
 
 class RarePatternDetect:
-    def __init__(self, delta, tau, epsilon, backend):
+    def __init__(self, delta, tau, epsilon, pattern_space):
         self.training_data = None
-        self.delta = delta,
-        self.tau = tau,
+        self.delta = delta
+        self.tau = tau
         self.epsilon = epsilon
-        if backend == "minlp":
-            self.has_rare_pattern = has_rare_pattern
+        self.pattern_space = pattern_space
+
+        if pattern_space.type == PatternSpaceType.AXIS_ALIGNED_HYPER_RECTANGLES:
+            self.has_rare_pattern = minlp_has_rare_pattern
         else:
             self.has_rare_pattern = None
 
@@ -17,7 +20,5 @@ class RarePatternDetect:
 
     def is_anomalous(self, x):
         return self.has_rare_pattern(
-            x,
-            self.training_data,
-            self.tau + self.epsilon/2
+            x, self.training_data, self.pattern_space, self.tau + self.epsilon / 2
         )
