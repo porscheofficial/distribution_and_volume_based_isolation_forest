94c7f7717494cc4a6c7bcc74b13ce030ce2da332
paul <paul.boes@porsche.digital>
2022-12-02 14:20:13 +0100

    added min_area calculation

--- rare_pattern_detect/rare_pattern_detect.py
+++ rare_pattern_detect/rare_pattern_detect.py
@@ -1,9 +1,9 @@
-from rare_pattern_detect.patterns import PatternSpaceType
+from rare_pattern_detect.patterns import PatternSpaceType, PatternSpace
 from rare_pattern_detect.minlp_based import minlp_has_rare_pattern
 
 
 class RarePatternDetect:
-    def __init__(self, delta, tau, epsilon, pattern_space):
+    def __init__(self, delta, tau, epsilon, pattern_space: PatternSpace):
         self.training_data = None
         self.delta = delta
         self.tau = tau
@@ -12,14 +12,20 @@ class RarePatternDetect:
 
         if pattern_space.type == PatternSpaceType.AXIS_ALIGNED_HYPER_RECTANGLES:
             self.has_rare_pattern = minlp_has_rare_pattern
+
         else:
             self.has_rare_pattern = None
 
     def load_training_data(self, training_data):
         self.training_data = training_data
-
-    def is_anomalous(self, x):
-        _, pred = self.has_rare_pattern(
-            x, self.training_data, self.pattern_space, self.tau + self.epsilon / 2
+        N, d = training_data.shape
+        self.pattern_space.calculate_coeff(
+            epsilon=self.epsilon, delta=self.delta, N=N, d=d
         )
-        return pred
+
+
+def is_anomalous(self, x):
+    _, pred = self.has_rare_pattern(
+        x, self.training_data, self.pattern_space, self.tau + self.epsilon / 2
+    )
+    return pred
