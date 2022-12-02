39e3a3a25b498022d8549548f422c056250edd8b
paul <paul.boes@porsche.digital>
2022-12-02 14:20:13 +0100

    added min_area calculation

--- rare_pattern_detect/patterns.py
+++ rare_pattern_detect/patterns.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math
 
 
 class PatternSpaceType(Enum):
@@ -10,3 +11,13 @@ class PatternSpace:
     def __init__(self, type: PatternSpaceType, cutoff: float = 0):
         self.type = type
         self.cutoff = cutoff
+
+    def calculate_coeff(self, **kwargs):
+        if self.type == PatternSpaceType.AXIS_ALIGNED_HYPER_RECTANGLES:
+            epsilon = kwargs["epsilon"]
+            delta = kwargs["delta"]
+            N = kwargs["N"]
+            d = kwargs["d"]
+            v = 2 * d
+            min_area = 1 / N * math.log(d)  # TODO: FIll in details
+            self.cutoff = min_area
