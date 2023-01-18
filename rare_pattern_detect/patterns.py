f81cdd1f848b79cffe0ae4d1441aa5aeee158412
HichDh <hichem.dhouib.tn@gmail.com>
2023-01-18 15:10:47 +0100

    some experiments with different min areas

--- rare_pattern_detect/patterns.py
+++ rare_pattern_detect/patterns.py
@@ -6,7 +6,7 @@ from sympy import solve_rational_inequalities, Poly
 
 # from rare_pattern_detect.minlp_based import MIN_AREA
 
-MIN_AREA = 1  # 0.00001 # 3.9 # 0.1
+# MIN_AREA = 1  # 0.00001 # 3.9 # 0.1
 
 
 class PatternSpaceType(Enum):
@@ -18,7 +18,7 @@ class PatternSpace:
     def __init__(self, type: PatternSpaceType, cutoff):
         self.type = type
         self.cutoff = cutoff if cutoff != None else self.calculate_coeff()
-        print("cutoff (min_area): ", self.cutoff)
+        print("cutoff (patterns.py): ", self.cutoff)
 
     def calculate_coeff(self, **kwargs):
         if self.type == PatternSpaceType.AXIS_ALIGNED_HYPER_RECTANGLES:
@@ -27,7 +27,9 @@ class PatternSpace:
             N = kwargs["N"]
             d = kwargs["d"]
             v = 2 * d
-            # TODO: min area can be solved with wolfram alpha and then plugged in here
-            # OR using simpy to solve the equation directly in python
-            # min_area = math.sqrt((1 / N) * (256 / epsilon**2) * ( v * math.log(256 / epsilon**2) + math.log(8 / delta)))
-            self.cutoff = 0.1  # MIN_AREA
+            # min area can be solved with wolfram alpha and then plugged in here
+            # TODO: using simpy to solve the equation directly in python
+            # self.cutoff = 6.3  # 0917
+            # using 63.0917 for 100 points -> infeasibility detected in deactivate_trivial_constraints
+            # Feasibility subproblem infeasible. This should never happen. -> all f_hats are zero
+            # return self.cutoff
