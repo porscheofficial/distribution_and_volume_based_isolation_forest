c4c6cb7eab074892f926674f41f37fa32b7ee736
hichem.dhouib1 <hichem.dhouib@porsche.digital>
2023-06-27 11:34:08 +0200

    renaming package to pac_dist_iforest

---
+++ src/pac_dist_iforest/__init__.py
@@ -0,0 +1,5 @@
+# from depth_based_renyi_isolation_forest import DepthBasedRenyiIsolationForest
+# from pac_based_renyi_isolation_forest import PACBasedRenyiIsolationForest
+#
+#
+# __all__ = ["PACBasedRenyiIsolationForest", "DepthBasedRenyiIsolationForest"]
